863591ad1413495d6e071e47b19b04cfcef5ff43
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-07-28 10:02:04 +0700

    FIX add imports to index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import "./styles.css";
 import capitalize from "./capitalize.js";
 import reverseString from "./reverseString.js";
 import calculator from "./calculator.js";
+import caesarCipher from "./caesarCipher.js";
+import analyzeArray from "./analyzeArray.js";
